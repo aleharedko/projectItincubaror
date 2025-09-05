8b396d800391b75743d9275f5593f43eb097004a
Aliaksei Redzko <aleha.redko@gmail.com>
2025-09-05 21:12:42 +0300

    add structure JS project syte and folder

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 'use strict'
+const MAX_QUANTITY_LETTERS = 50
+
+function countingNotes(){
+    
+}
+
+function inpuAbove () {
+
+}
+
+function inpuABelove () {
+    
+}
+
+function butnEvent (){
+
+}
+
+function errorMasage(){
+
+}
+
+function fullAccsesMassage(){
+
+}
