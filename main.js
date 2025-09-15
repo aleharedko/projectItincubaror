64fb1c10e88efc54b813a8416b4ccefa76f01b44
Aliaksei Redzko <aleha.redko@gmail.com>
2025-09-15 21:12:36 +0300

    restart project

--- main.js
+++ main.js
@@ -1,72 +1,23 @@
 'use strict'
 const MAX_QUANTITY_LETTERS = 50  //максимальное кол-во для ввода в верхнем инпуте
 
-const buttonQS = document.querySelector(".btn")
-const checkboxQS = document.querySelector(".cb_click")
-const textContainerAboveInput = document.querySelector(".above_input")
-const counerNote = document.querySelector(".counter")
-
-counerNote.textContent = "0"
 
 const MOKE_NOTE = [
-    {},
     {}
 ]
 
 const model = { // хранение данных, бизнес-логика
-note: MOKE_NOTE
+
 }
 
 const view = { // отображение данных: рендер списка задач, размещение обработчиков событий 
+init(){
 
+}
 }
 
 const controler = { // обработка действий пользователя, обновление модели
 
 }
 
-
-
-
-function countingNotes(fnClick, fnDelet) { //счетчик
-    let count = 0
-    counerNote.textContent = count
-    if (fnClick) {
-        count++
-    } else if (fnDelet) {
-        count--
-    }
-    return count
-}
-
-countingNotes(btnClick(), )
-
-let getTextAboveIput = function inpuAbove(fn) {  //поледя название заметки
-    if (textContainerAboveInput >= MAX_QUANTITY_LETTERS) {
-        alert("More simbele")
-    }
-}
-
-function inputBelove(textContens, nameContent, favoriteItem, deletedFn) { // поле для текста
-    const inputArea = document.createElement("div")
-    inputArea.classList.add("new_note")
-    inputArea.textContent = textContens
-}
-inputBelove()
-
-const btnClick = function butnEvent(fnAcsees) { //кнопка
-    buttonQS.addEventListener("click", function () {
-
-
-    })
-}
-
-butnEvent(fullAccsesMassage())
-
-const eroorFn = function errorMasage() { //неудачное добавление
-    alert("Невозможно добавить заметку")
-}
-
-function fullAccsesMassage() { //удачное
-    alert("Заметка добавлен!")
-}
+init()
