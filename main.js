da65962653038d4c368ed9ad534a61f93be02b45
Aliaksei Redzko <aleha.redko@gmail.com>
2025-09-22 21:37:42 +0300

    work with project

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
 'use strict'
-const MAX_QUANTITY_LETTERS = 50  //максимальное кол-во для ввода в верхнем инпуте
+const MAX_QUANTITY_LETTERS = 50 //максимальное кол-во для ввода в верхнем инпуте
+const counterNumber = document.querySelector('.counter')
 
 const model = { // хранение данных, бизнес-логика
     task: [{}, //мои моковые данные
-        {}]
+        {}], // <--- Пропущена запятая
+    // newTask: {id, isDone, title}, // <--- Нельзя объявлять переменную внутри объекта
 
- const newTask = {id, isDone, title}
+    // Правильный способ хранить данные:
+    newTask: {id: null, isDone: false, title: ''}
 }
 
-const view = { // отображение данных: рендер списка задач, размещение обработчиков событий 
-init(){
-    this.renderTask(model.task)
+const view = { // отображение данных: рендер списка задач, размещение обработчиков событий
+    init(){
+        counterNumber.innerHTML = '0'
+        this.renderTask(model.task)
+    }, // <--- Пропущена запятая
 
-    renderTask(){ //функция для рендаринга
-
+    renderTask(tasks){ //функция для рендеринга
+        // Логика рендеринга
     }
 }
+
+const controller = { // обработка действий пользователя, обновление модели
+    init() {
+        // Логика инициализации контроллера
+    }
 }
 
-const controler = { // обработка действий пользователя, обновление модели
-
-}
-
-init()
+// Теперь можно вызывать методы объектов
+view.init()
+controller.init()
