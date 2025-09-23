d6ebe1c4dbb4ea48ef7fcabd29b1ef0c64e1afb5
Aliaksei Redzko <aleha.redko@gmail.com>
2025-09-23 15:03:49 +0300

    work with project adds more line code

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict'
 const MAX_QUANTITY_LETTERS = 50 //максимальное кол-во для ввода в верхнем инпуте
-const counterNumber = document.querySelector('.counter')
 
 const MOCK_NOTES = [
     {
@@ -25,35 +24,64 @@ const colors = {
     PURPLE: 'purple',
 }
 
-const MOCK_NOTES = [
-    {
-        // ...
-        color: colors.GREEN,
-    },
-    {
-        // ...
-        color: colors.YELLOW,
-    }
-]
+// const MOCK_NOTES = [
+//     {
+//         // ...
+//         color: colors.GREEN,
+//     },
+//     {
+//         // ...
+//         color: colors.YELLOW,
+//     }
+// ]
 
 const view = {
     init() {
         this.renderNotes(model.notes)
     },
     renderNotes(notes) {
-        // your code here
         // находим контейнер для заметок и рендерим заметки в него (если заметок нет, отображаем соответствующий текст)
+        const listContainer = document.querySelector(".notes-list")
+
+        if(!listContainer){//Проверяем, что контейнер найден, чтобы избежать ошибок.
+            return;
+        }
+        listContainer.innerHTML = ''; //чтобы заметки не дублировались при повторном вызове функции
+
+        if (notes.length === 0){
+             listContainer.innerHTMl =
+                 `
+             <p>У вас ещё нет ни одной заметки. Заполните поля выше и создайте свою первую заметку!</p>;
+             `
+            return;
+        }
+
+        notes.forEach(note => {
+            const noteElement = document.createElement('div')
+            noteElement.className = 'note-item'
+            noteElement.innerHTML = `
+             <h3>${note.title}</h3>
+                <p>${note.content}</p>
+                `;
+
+            // Добавляем созданный элемент в основной контейнер.
+            listContainer.appendChild(noteElement);
+        })
+
         // также здесь можно будет повесить обработчики кликов на кнопки удаления и избранного
 
     },
-    renderNotesCount(){  //функция для отображения счетчика с количеством заметок
 
+    renderNotesCount(){
+        const numberCount = model.notes.length
+        const count = document.querySelector(".strong_number")
+        count.innerHTML = numberCount
     }
 }
 
 function init() {
     view.init()
-    view.init.renderNotesCount()
+    view.renderNotesCount()
 
 }
 
