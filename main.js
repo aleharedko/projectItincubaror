9eab572c59f8150d98fc9559c1de433e9c43645c
Aliaksei Redzko <aleha.redko@gmail.com>
2025-10-14 23:02:20 +0300

    fsf

--- main.js
+++ main.js
@@ -110,6 +110,8 @@ const view = {
                         123
             11111
 
+            1111
+
             // <div class="note_header" style="background-color: ${note.color};">
             //     <h3 class="note_title">${note.title}</h3>
             //     <div class="note_actions">
